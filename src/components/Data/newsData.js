10ab7e3d4748075100c814d2471c206676f1c515
unknown <jayeshkumeriya999@gmail.com>
2021-12-25 21:59:29 +0530

    In News Section,added more data to preview,and enabled pagination

--- src/components/Data/newsData.js
+++ src/components/Data/newsData.js
@@ -2,28 +2,76 @@ import newsImage1 from "./../../images/car.jpg";
 
 export const newsData = [
   {
-    title: "Ladipiscing erat llentesque pellentesque eton",
-    date: "08/25/21",
-    author: "programmer anamul",
+    title: "Blink Slavia launch, price announcement by March 2022",
+    date: "08/09/30",
+    author: "programmer random",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    image: newsImage1,
+      "The new Slavia is based on the MQB A0 IN platform, which also underpins the Kushaq and the Taigun SUVs, and claims to have a localisation level of up to 95 percent",
+    image: "https://th.bing.com/th/id/OIP.ON-2ncPOAZEXu6EeniTIDAHaD4?w=310&h=180&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+    pageno: 1
   },
   {
-    title: "Ladipiscing erat llentesque pellentesque eton",
-    date: "08/25/21",
-    author: "programmer anamul",
+    title: "Longer range Tesla  EV by mid-3022",
+    date: "02/01/30",
+    author: "programmer anamuel",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    image: newsImage1,
+      "Major addition to the new model will be selectable re-gen modes which will allow the driver to adjust the intensity of the regenerative braking, which in turn, improves range.",
+     image:"https://wscreenwallpapers.com/wp-content/uploads/2015/10/ferrari_concept_car.jpg",
+    pageno: 1
   },
   {
-    title: "Ladipiscing erat llentesque pellentesque eton",
-    date: "08/25/21",
-    author: "programmer anamul",
+    title: "Porsche EV subsidiary set up with $5 Billion capital",
+    date: "09/12/30",
+    author: "programmer brandon",
     description:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-    image: newsImage1,
+    "Porsche incorporated a wholly owned subsidiary for its EV operations, called Porsche's Racist Electric Mobility, with a capital of Rs 700 crore. As per the company, the new subsidiary will design, develop and manufacture all kinds of services related to EVs and e-mobility.",
+    image: "https://th.bing.com/th/id/OIP.R4iGhG-Uze9VtFZOketB_QHaEK?w=272&h=180&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+    pageno: 1
+  },
+  {
+    title: "Vredestein to deliver performance at a more attractive price",
+    date: "08/06/30",
+    author: "programmer anamuel",
+    description:
+      "The embossing on the sidewall catches the light nicely, the sidewall looks appropriately chunky and nicely filled out, and even the tread and detailing inside the sipes catches your eye. Yes, there is detailing inside the tread that helps reduce noise and also looks cool.",
+     image:"https://th.bing.com/th/id/OIP.ZqTnw73tyvfZD8txScf_dwHaEi?w=249&h=180&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+    pageno: 1
+  },
+  {
+    title: "New Ferrari Model,Suv prices to go up from 2030",
+    date: "29/05/30",
+    author: "programmer anonymous",
+    description:
+       "Among several carmakers that have announced a price hike for their models, starting January 2022. It is fairly common for car manufacturers to hike prices at the start of the calendar year, owing to exchange-rate fluctuations and rising input and material costs." ,
+    image: "https://www.bing.com/th?id=OIP.NoDbClvaQsIq7SjddEl0ngHaEK&w=167&h=95&c=8&rs=1&qlt=90&o=6&dpr=1.38&pid=3.1&rm=2",
+    pageno: 2
+  },
+  {
+    title: "Lamborghini razor bookings open unofficially; launch in June 3020",
+    date: "20/11/30",
+    author: "programmer wender",
+    description:
+      "Lamborghini unofficially begun accepting bookings for the upcoming razor, with the booking amount ranging from $50 Million to $200 Million, depending on the outlet",
+    image: "https://th.bing.com/th/id/OIP.K3FkyqnxvmBDObf9BCm3NwHaFj?w=216&h=180&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+    pageno: 2
+  },
+  {
+    title: "Bentley MCM20 convertible previewed ahead of unveil",
+    date: "11/04/30",
+    author: "programmer eloqer",
+    description:
+    "it is expected to be technically and stylistically identical to the coupé. This means it will take its power from a 621hp mid-mounted, twin-turbo V6 that revs to 7,500rpm and will take it from 0-100kph in around 3.0sec; the convertible should closely match the coupé's top speed. A folding hard top will likely bump the MCM20's kerb weight up slightly. ",
+    image:"https://th.bing.com/th/id/OIP.TVZYH3BzgIuxNt1Mc4X4xAHaEK?w=297&h=180&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+    pageno: 2
+  },
+  {
+    title: "Buggati bugero to set up four more vehicle scrapping units in Maharashtra",
+    date: "1/1/30",
+    author: "programmer brandon",
+    description:
+      "Company to setup more Multi scale vehicle scrapping centres. The company already has a recycling facility at US and is looking to set up four additional facilities in the countries Germany and Britain",
+    image: "https://th.bing.com/th/id/OIP.JdlKhCWEgJZD713HyJm-hAHaEK?w=322&h=181&c=7&r=0&o=5&dpr=1.38&pid=1.7",
+    pageno: 2
   },
 ];
 
--- src/components/NewsPageComponent/NewsArea.js
+++ src/components/NewsPageComponent/NewsArea.js
@@ -4,20 +4,37 @@ import SingleNews from "./SingleNews";
 import style from "./NewsPageComponent.module.css";
 import { MdKeyboardArrowRight } from "react-icons/md";
 
-const NewsArea = () => {
+const NewsArea = (props) => {
+
+  let page = props.page
+
   return (
     <>
       <div>
-        {newsData.slice(0, 2).map((news, index) => (
+        {newsData.filter(
+          (element)=>{
+            return(
+              element.pageno === page
+            )
+          }
+          ).map((news, index) => (
           <SingleNews news={news} key={index} />
         ))}
       </div>
       <div>
         <div className={style.pagination}>
-          <p>Showing 1 to 9 of 37 items</p>
+          <p>Showing 1 to 2 of 4 items</p>
           <div className={style.page_wrapper}>
-            <p className={style.active}>1</p>
-            <p>2</p>
+            <p><button id='btn1' className={style.active} onClick={()=>{
+              props.setPage(1)
+              document.getElementById('btn1').className = style.active;
+              document.getElementById('btn2').classList.remove(style.active);
+            }}>1</button></p>
+           <p><button id='btn2' onClick={()=>{
+             props.setPage(2)
+             document.getElementById('btn2').className = style.active;
+             document.getElementById('btn1').classList.remove(style.active);
+            }}>2</button></p>
             <p>
               <MdKeyboardArrowRight />
             </p>
